2bef9a77f371d29017493c91979591a9dbd8dc62
christophebe <christophe.lombart@gmail.com>
2015-10-05 08:24:46 +0200

    add comment

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var fs     = require("fs")
 var bunyan = require("bunyan");
 
+/**
+ *  Common method to log used by the crawler and if needed by plugins
+ *  Ideally, the log json structure should contains the following attributes :
+ *  - url (can be null for specific crawl steps).
+ *  - step
+ *  - message
+ *  - options
+ *
+ * @return
+ */
 
 var infoFile = process.cwd() + "/logs/crawler.log";
 var debugFile = process.cwd() + "/logs/debug.log";
@@ -8,6 +18,11 @@ var debugFile = process.cwd() + "/logs/debug.log";
 console.log("Use info log in : " + infoFile);
 console.log("Use debug log in : " + debugFile);
 
+
+/**
+ *  Default logger
+ *
+ */
 var Logger = bunyan.createLogger({
   name: 'full-log',
   streams: [
@@ -27,7 +42,14 @@ var Logger = bunyan.createLogger({
   ]
 });
 
-
+/**
+ * If needed, a plugin can create a specific log
+ *
+ *
+ * @param the name of the Logger
+ * @param the json config structure (see bunyan documemntion )
+ * @return the new logger
+ */
 var createLogger = function(name, streamInfo) {
   return bunyan.createLogger({
     name: name,
